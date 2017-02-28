e4af647f99aed7e3112360f1e4f365ccec16e175
yOoMarvin <marvin.messenzehl@gmail.com>
2017-02-28 16:14:56 +0100

    Added controller for collection view and the collection view cell

---
+++ reader/Article.swift
@@ -0,0 +1,15 @@
+//
+//  Article.swift
+//  reader
+//
+//  Created by Marvin Messenzehl on 28.02.17.
+//  Copyright © 2017 Marvin Messenzehl. All rights reserved.
+//
+
+import Foundation
+
+struct Article {
+    var title = ""
+    var imageUrl = ""
+    var url = ""
+}
---
+++ reader/ArticleCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleCollectionViewCell.swift
+//  reader
+//
+//  Created by Marvin Messenzehl on 28.02.17.
+//  Copyright © 2017 Marvin Messenzehl. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+}
